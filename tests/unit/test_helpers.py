d9ae8232c4ebd148dfe277477506aa271f31cf74
astephensVM <astephens16@wou.edu>
2021-01-19 21:40:15 -0500

    added test for app.helper.pretty_date.days_ago TC:91%

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 
 from app.helpers import pretty_date
 
-
 def test_yesterday():
-    assert (pretty_date(datetime.utcnow() - timedelta(days=1))) == "Yesterday"
+    assert (pretty_date(datetime.utcnow() - timedelta(days = 1))) == "Yesterday"
+
+def test_days_ago():
+    assert (pretty_date(datetime.utcnow() - timedelta(days = 6))) == "6 days ago"
